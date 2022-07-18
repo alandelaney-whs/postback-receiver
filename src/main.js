f38ee289d5de21a5afa94bdc9e67aa96a02ef8e3
Delaney, Alan <67151449+alandelaney-whs@users.noreply.github.com>
2022-07-18 11:33:56 +0100

    Add standard package and run linter

--- src/main.js
+++ src/main.js
@@ -1,106 +1,101 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {readFile} from 'fs/promises';
-import {getRandomInt} from './utils/utils.js';
-import {numberDetect as typeOf} from 'number-detect'
+import express from 'express'
+import bodyParser from 'body-parser'
+import { readFile } from 'fs/promises'
+// import { getRandomInt } from './utils/utils.js'
+import { numberDetect as typeOf } from 'number-detect'
 
 let requestCount = 0
 
 const config = JSON.parse(
-    await readFile(
-        new URL('../config/config.json', import.meta.url)
-    )
-);
+  await readFile(
+    new URL('../config/config.json', import.meta.url)
+  )
+)
 
 const main = (cfg) => {
-    const app = express();
-    app.use(express.json())
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
+  const app = express()
+  app.use(express.json())
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
 
-    // Read config values with hard-coded defaults
-    const port = cfg.port || 3000;
+  // Read config values with hard-coded defaults
+  const port = cfg.port || 3000
 
-    app.get("/postback", normalServer)
-    app.post("/postback", normalServer)
-    app.get("/queue", queueServer)
-    app.post("/queue", queueServer)
-    app.get("/broken", brokenServer)
-    app.post("/broken", brokenServer)
-    app.get("/lazy", lazyServer)
-    app.post("/lazy", lazyServer)
-    app.get("/unresponsive", unresponsiveServer)
-    app.post("/unresponsive", unresponsiveServer)
-    app.get("/rude", rudeServer)
-    app.post("/rude", rudeServer)
+  app.get('/postback', normalServer)
+  app.post('/postback', normalServer)
+  app.get('/broken', brokenServer)
+  app.post('/broken', brokenServer)
+  app.get('/lazy', lazyServer)
+  app.post('/lazy', lazyServer)
+  app.get('/unresponsive', unresponsiveServer)
+  app.post('/unresponsive', unresponsiveServer)
+  app.get('/rude', rudeServer)
+  app.post('/rude', rudeServer)
 
-    // Default route
-    app.all("*", (req, res) => {
-        console.log("Rejecting invalid route")
-        res.status(404).send("PAGE NOT FOUND");
-    });
+  // Default route
+  app.all('*', allRoutes)
 
-    app.listen(port, () => {
-        console.log(`Postback receiver listening at http://localhost:${port}/postback`)
-    });
-};
+  app.listen(port, () => {
+    console.log(`Postback receiver listening at http://localhost:${port}/postback`)
+  })
+}
 
 // A normal server acknowledges the postback with a 201 status
 const normalServer = (req, res) => {
-    console.log(`Postback endpoint contacted at ${new Date()}`)
+  console.log(`Postback endpoint contacted at ${new Date()}`)
 
-    if (typeOf(req.body) === 'Object' && typeOf (req.body.data) === 'Array') {
-        console.log(`    req.body.data is an Array with ${req.body.data.length} element(s)`)
-        console.log(`    Metadata = ${JSON.stringify(req.body, null, 2)}`)
-    } else {
-        console.log('    req.body.data is not an Array')
-    }
+  if (typeOf(req.body) === 'Object' && typeOf(req.body.data) === 'Array') {
+    console.log(`    req.body.data is an Array with ${req.body.data.length} element(s)`)
+    console.log(`    Metadata = ${JSON.stringify(req.body, null, 2)}`)
+  } else {
+    console.log('    req.body.data is not an Array')
+  }
 
-    if (req.body && req.body.metadata) {
-        console.log(`    Metadata = ${JSON.stringify(req.body.metadata)}`)
-        //console.log(`    Metadata = ${req.body.metadata}`)
-    } else {
-        console.log('    No metadata found')
-    }
+  if (req.body && req.body.metadata) {
+    console.log(`    Metadata = ${JSON.stringify(req.body.metadata)}`)
+    // console.log(`    Metadata = ${req.body.metadata}`)
+  } else {
+    console.log('    No metadata found')
+  }
 
-    setTimeout(() => res.status(201).send("Postback acknowledged"), 500)
-}
-
-const queueServer = (req, res) => {
-    console.log(`Queue endpoint contacted at ${new Date()}`)
-    console.log(JSON.stringify(req.body, null, 2))
-
-    setTimeout(() => res.status(200).send("Postback acknowledged"), 500)
+  setTimeout(() => res.status(201).send('Postback acknowledged'), 500)
 }
 
 // A broken server responds with a 500 error code
 const brokenServer = (req, res) => {
-    console.log(`Broken endpoint contacted at ${new Date()}`)
-    setTimeout(() => res.status(500).send("An internal error occurred"), 500)
+  console.log(`Broken endpoint contacted at ${new Date()}`)
+  setTimeout(() => res.status(500).send('An internal error occurred'), 500)
 }
 
 // A lazy server accepts the first two responses, then decides it can't be bothered anymore
 const lazyServer = (req, res) => {
-    console.log(`Lazy endpoint contacted at ${new Date()}`)
-    requestCount++
+  console.log(`Lazy endpoint contacted at ${new Date()}`)
+  requestCount++
 
-    if (requestCount > 2) {
-        setTimeout(() => res.status(204).send("I've had enough now"), 500)
-    } else {
-        setTimeout(() => res.status(201).send("Postback acknowledged"), 500)
-    }
+  if (requestCount > 2) {
+    setTimeout(() => res.status(204).send("I've had enough now"), 500)
+  } else {
+    setTimeout(() => res.status(201).send('Postback acknowledged'), 500)
+  }
 }
 
 // An unresponsive server keeps the connection open for 2 minutes, or until the client takes the hint
 const unresponsiveServer = (req, res) => {
-    console.log(`Unresponsive endpoint contacted at ${new Date()}`)
-    setTimeout(() => res.status(201).send("Postback acknowledged"), 120000)
+  console.log(`Unresponsive endpoint contacted at ${new Date()}`)
+  setTimeout(() => res.status(201).send('Postback acknowledged'), 120000)
 }
 
 // A rude server hangs up without replying
 const rudeServer = (req, res) => {
-    console.log(`Rude endpoint contacted at ${new Date()}`)
-    setTimeout(() => req.socket.destroy(), 500)
+  console.log(`Rude endpoint contacted at ${new Date()}`)
+  setTimeout(() => req.socket.destroy(), 500)
 }
 
-main(config);
+const allRoutes = (req, res) => {
+  console.log(`General endpoint contacted at ${new Date()}`)
+  console.log(JSON.stringify(req.body, null, 2))
+
+  setTimeout(() => res.status(200).send('Postback acknowledged'), 500)
+}
+
+main(config)
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,17 +1,17 @@
 /* eslint-disable no-eq-null */
-export const isNull = x => x == null;
-export const isNotNull = x => x != null;
+export const isNull = x => x == null
+export const isNotNull = x => x != null
 /* eslint-enable no-eq-null */
 
-export const isError = x => Object.prototype.toString.call(x) === '[object Error]';
+export const isError = x => Object.prototype.toString.call(x) === '[object Error]'
 
 // throwError takes an error message string and wraps it in a function that throws
 // that error when called.
 export const throwError = message => () => {
-    throw new Error(message);
-};
+  throw new Error(message)
+}
 
 // getRandomInt generates a function that returns a random number in a given range.
 export const getRandomInt = (min, max) => () => (
-    Math.floor(Math.random() * (max - min + 1)) + min
-);
+  Math.floor(Math.random() * (max - min + 1)) + min
+)
